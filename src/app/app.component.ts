18bed03b6af0b4477e762a2503a03e8f5457942d
sarahlia <61435406+sarahlia@users.noreply.github.com>
2020-10-14 23:07:20 -0500

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+import { TranslateService } from "./translate.service";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  text: string;
+  translatedText: any;
+
+  constructor(private translateService: TranslateService) {}
+
+  submit() {
+    this.translateService.translate(this.text).subscribe((result) => {
+      this.translatedText = result;
+    });
+  }
+}
